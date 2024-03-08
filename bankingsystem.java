2ebe6c97eef8adea50850194a1c02284bf26fbd3
Jorge Fuertes <70719833+horhebush@users.noreply.github.com>
2024-03-08 09:50:10 +0800

    Update bankingsystem.java
    added pin validation method

--- bankingsystem.java
+++ bankingsystem.java
@@ -4,7 +4,33 @@ import java.awt.Toolkit;
 import java.util.ArrayList;
 public class BankingSystem {
 
+	private static String storedPin;
+	private static String storedName;
+	private static String storedUser;
+	private static String storedPass;
+
 	public static void main(String[] args) {
 
+	}
+
+	private static boolean PinValidation(Scanner pin)
+	{	
+		for (int x = 0; x < 3; x++) {
+	        System.out.print("\nAccount Pin: ");
+	        String enteredPin = pin.nextLine();
+
+	        if (enteredPin.equals(storedPin)) 
+	        {
+	            return true;
+	        } 
+	        else 
+	        {
+	            System.out.println("\n\tWrong Passcode. Please try again.\n");
+	        }
+	    }
+
+	    System.out.println("You have been logged out!");
+	    Open();
+	    return false;
 	}
 }
