770ca7cebeb1d073a5baa5b1fe82fd585f322d1b
Jorge Fuertes <70719833+horhebush@users.noreply.github.com>
2024-03-09 14:24:16 +0800

    final changes
    we got a 100

--- bankingsystem.java
+++ bankingsystem.java
@@ -1,10 +1,7 @@
-//Calvo, Jezlyn Mae               //Nofre, Jasmine
-//Fuertes, Jorge Omar		 //Salazar, Cheska Ann Lorein
-//Lim, Lorenzo Matthew		//Tuazon, Jamaine Grace
-
-package Final_Project;
+package Laboratory;
+import java.awt.Toolkit;
 import java.util.Scanner;
-public class BankingSystem {
+public class FinalProject {
 
     private static String storedName;
     private static String storedUser;
@@ -22,40 +19,28 @@ public class BankingSystem {
     }
 
     private static void homePage() {
-        char option;
 
-        do {
-            System.out.println("\n\t\tWelcome to Federal Reserve Bank of COM23P!");
-            System.out.println("\n\t\t[A] Sign Up");
-            System.out.println("\t\t[B] Log In");
-            System.out.println("\t\t[X] Exit");
-            System.out.print("\t\tChoose an option: ");
-            option = scn.next().charAt(0);
-            scn.nextLine();
+    	System.out.println("\n\tFederal Reserve Bank of COM23P\t\n");
+		System.out.println("\t\t[A] Sign Up\t");
+		System.out.println("\t\t[B] Log In\t");
+		System.out.println("\t\t[X] Exit\t");
+		
+		char x = Default();
 
-            switch (option) {
-                case 'A', 'a':
-                    System.out.println("\n\tSign Up selected. ");
-                    signUp();
-                    break;
-                case 'B', 'b':
-                    System.out.println("\n\tLog In selected. ");
-                    LogIn();
-                    break;
-                case 'X', 'x':
-                    System.out.println("\n\t\tLeaving the Banking System. Goodbye! ");
-                    return;
-                default:
-                    System.out.println("\n\t\tInvalid option. Please try again. ");
-                    break;
-            }
-        } while (option != 'X');
+		switch(x)
+		{
+		case 'A', 'a': signUp(); break;
+		case 'B', 'b': LogIn(); break;
+		case 'C', 'c': homePage(); break;
+		case 'X', 'x': MethodX(); break;
+		}
     }
 
     private static void signUp() {
-        System.out.println("\tCreate an Account!\n");
+        System.out.println("\n\tCreate an Account!\n");
         System.out.print("\tEnter Account Name: ");
         storedName = scn.nextLine();
+        scn.nextLine();
 
         System.out.print("\tEnter PIN: ");
         storedPin = scn.nextInt();
@@ -66,10 +51,12 @@ public class BankingSystem {
 
         System.out.print("\tEnter Password: ");
         storedPass = scn.nextLine();
+        
+        homePage();
     }
 
     private static void LogIn() {
-        System.out.println("\tYou may now log in your account.\n");
+        System.out.println("\n\tYou may now log in your account.\n");
         System.out.print("\n\tEnter Your Username: ");
         String username = scn.nextLine();
         System.out.print("\tEnter Your Password: ");
@@ -86,135 +73,126 @@ public class BankingSystem {
         }
     }
 
-    private static void menu() {
-        char choice, choiceC;
+    private static void menu() 
+    {
+    	System.out.println("\n\t\tHello, " + storedName + "!");
+        System.out.println("\tWelcome to Federal Reserve Bank of COM23P!");
+        System.out.println("\n\tBank Account Menu:\n");
+        System.out.println("\t[A] Money Transaction");
+        System.out.println("\t[B] Balance");
+        System.out.println("\t[C] Account Details");
+        System.out.println("\t[D} Account Transaction");
+        System.out.println("\t[X] Log Out");
 
-        do {
-            System.out.println("\n\tHello, " + storedName);
-            System.out.println("\tWelcome to Federal Reserve Bank of COM23P!");
-            System.out.println("\n\tBank Account Menu:");
-            System.out.println("\t[A] Transaction Menu");
-            System.out.println("\t[B] Balance");
-            System.out.println("\t[C] Account Details");
-            System.out.println("\t[D} Account Transaction");
-            System.out.println("\t[X] Log Out");
-            System.out.print("\tEnter your choice: ");
-            choice = scn.next().charAt(0);
-            scn.nextLine();
+        char x = Default();
 
-            switch (choice) {
-                case 'A', 'a':
-                	System.out.println("\n\tTransaction Menu:");
-                	System.out.println("\t[A] Deposit");
-                	System.out.println("\t[B] Withdraw");
-                	System.out.println("\t[X] Exit");
-                	System.out.print("\tEnter your choice: ");
-                	choiceC = scn.next().charAt(0);
-                    scn.nextLine();
-                	
-                    switch (choiceC) {
-                		case 'A' , 'a':
-                			deposit();
-                			break;
-                		case 'B' , 'b':
-                			withdraw();
-                			break;
-                		case 'X' , 'x':
-                			break;
-                		default:
-                			System.out.println("\n\tInvalid choice. Please try again.");
-                            break;
-                	}
-                    break;
-                case 'B', 'b':
-                    checkBalance();
-                    break;
-                case 'C', 'c':
-                    displayAccountDetails();
-                    break;
-                case 'D', 'd':
-                	displayAccountTransaction();
-                	break;
-                case 'X', 'x':
-                    System.out.println("\n\tYou have successfully logged out!");
-                    return;
-                default:
-                    System.out.println("\n\tInvalid choice. Please try again.");
-                    break;
-            }
-        } while (choice != 'F');
+        switch(x)
+    	{
+    		case 'A', 'a': Money(); break;
+    		case 'B', 'b': checkBalance(); break;
+    		case 'C', 'c': displayAccountDetails(); break;
+    		case 'D', 'd': displayAccountTransaction(); break;
+    		case 'X', 'x': homePage(); break;
+    	}
     }
+    private static void Money()
+	{	
+        System.out.println("\n\tFederal Reserve Bank of COM23P!");
+		System.out.println("\n\t\t[A] Deposit\t");
+		System.out.println("\t\t[B] Withdraw\t");
+		System.out.println("\t\t[X] Menu\t\n");
+	
+		char x = Default();
 
-    private static void deposit() {
+		switch(x)
+		{
+		case 'A', 'a': deposit(); break;
+		case 'B', 'b': withdraw(); break;
+		case 'C', 'c': Money(); break;
+		case 'X', 'x': menu(); break;
+		}
+	}
+
+    private static void deposit() 
+    {
         double depAmount;
 
-        if (PinValidation()) {
-            System.out.print("\n\tEnter amount to deposit: ");
-            depAmount = scn.nextDouble();
+        PinValidation();
+        System.out.print("\n\tEnter amount to deposit: ");
+        depAmount = scn.nextDouble();
 
-            balance += depAmount;
-            balances[transactionCount++] = depAmount;
+        balance += depAmount;
+        balances[transactionCount++] = depAmount;
             
-            System.out.println("\tNew Balance: " + balance);
-        } else {
-            System.out.println("Invalid Credentials!");
-        }
+        System.out.println("\n\tNew Balance: " + balance);
+        Money();
     }
 
     private static void withdraw() {
         double withAmount;
 
-        if (PinValidation()) {
-            System.out.print("\n\tEnter amount to withdraw: ");
-            withAmount = scn.nextDouble();
+        PinValidation();
+        System.out.print("\n\tEnter amount to withdraw: ");
+        withAmount = scn.nextDouble();
 
-            if (balance < withAmount) {
-                System.out.println("\tInsufficient Funds");
-                return;
-            } else {
-                balance -= withAmount;
-                balances[transactionCount++] = -withAmount;
-
-                System.out.println("\tNew Balance: " + balance);
-            }
-        } else {
-            System.out.println("Invalid Credentials!");
+        if (balance < withAmount) 
+        {
+            System.out.println("\n\tInsufficient funds. Check your balance.");
+            menu();
+            return;
+        } 
+        else
+        {
+        	balance -= withAmount;
+        	balances[transactionCount++] = -withAmount;
+            System.out.println("\n\tNew Balance: " + balance);
         }
-    }
+        Money();
+     }
+ 
 
     public static void checkBalance() {
+    	PinValidation();
         System.out.println("\n\tCurrent balance: " + balance);
+        menu();
     }
 
     public static void displayAccountDetails() {
-        System.out.println("\n\tAccount Details:");
+    	PinValidation();
+        System.out.println("\n\tAccount Details: \n");
         System.out.println("\tAccount Name: " + storedName);
         System.out.println("\tPIN: " + storedPin);
         System.out.println("\tUsername: " + storedUser);
         System.out.println("\tPassword: " + storedPass);
         System.out.println(" ");
+        
+        menu();
     }
     
-    public static void displayAccountTransaction() {
+    public static void displayAccountTransaction() 
+    {
         
-        if (PinValidation()) { 
-            System.out.println("\n\tTransaction History");
+        PinValidation();
+        System.out.println("\n\tTransaction History\n");
 
-            for (int i = 0; i < transactionCount; i++) {
-                if (balances[i] > 0) {
-                    System.out.println("\tDeposited: " + balances[i]);
-                } else {
-                    System.out.println("\tWithdrawn: " + (-balances[i]));
-                }
-            }
-        } else {
-            System.out.println("Invalid Credentials!");
+        for (int i = 0; i < transactionCount; i++) 
+        {
+        	if (balances[i] > 0) 
+        	{
+        		System.out.println("\tDeposited: " + balances[i]);
+        	} 
+        	else 
+        	{
+        		System.out.println("\tWithdrawn: " + (-balances[i]));
+                
+        	}
         }
-    }
+        menu();
+    } 
 
     private static boolean PinValidation() {
         for (int x = 0; x < 3; x++) {
-            System.out.print("\tAccount Pin: ");
+            System.out.print("\n\tAccount Pin: ");
             int enteredPin = scn.nextInt();
 
             if (enteredPin == storedPin) {
@@ -224,9 +202,38 @@ public class BankingSystem {
             }
         }
         System.out.println("You have been logged out!");
+        Toolkit.getDefaultToolkit().beep();
         homePage();
         return false;
     }
+    
+	private static void MethodX() 
+	{
+		System.out.println("\n\tThank you for using Federal Reserve Bank of COM23P");
+		System.out.println("\t\t    Have a good one. Good bye!");
+	}
+    
+	private static char Default() 
+	{
+		char x;
+
+		Scanner select = new Scanner(System.in);
+		System.out.print("\nSelect Option: ");
+		x = select.next().charAt(0);
+		
+		do 
+		{
+			if (x != 'A' && x != 'B' && x != 'C' && x != 'D' && x != 'X' && x != 'a' && x != 'b' && x != 'c' && x != 'd' && x != 'x') 
+			{
+				System.out.println("\nInvalid Input\n");
+				System.out.print("Select Option: ");
+				x = select.next().charAt(0);
+			}
+		} 
+		
+		while (x != 'A' && x != 'B' && x != 'C' && x != 'D' && x != 'X' && x != 'a' && x != 'b' && x != 'c' && x != 'd' && x != 'x');
+		
+		return x;
+	}
 	
 }
-
